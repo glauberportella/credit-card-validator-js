560b32b8553ebe3e0b099ced1e5c71bbe45061fc
Glauber Portella <glauberportella@gmail.com>
2018-08-11 23:45:22 -0300

    removido obsoleto main.ts

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { AmexCreditCard } from './moip/creditcard/AmexCreditCard';
-import { Brands } from './moip/creditcard/Brands';
-import { DinersCreditCard } from './moip/creditcard/DinersCreditCard';
-import { EloCreditCard } from './moip/creditcard/EloCreditCard';
-import { HipercardCreditCard } from './moip/creditcard/HipercardCreditCard';
-import { HiperCreditCard } from './moip/creditcard/HiperCreditCard';
-import { MasterCreditCard } from './moip/creditcard/MasterCreditCard';
-import { VisaCreditCard } from './moip/creditcard/VisaCreditCard';
-import { CreditCard } from './moip/validators/CreditCard';
